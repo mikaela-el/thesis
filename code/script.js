ea755ed8a24e68ae5882a397b2b7786e2720264e
EC2 Default User <ec2-user@ip-172-31-90-182.ec2.internal>
2019-04-12 10:31:51 +0000

    updated folders

--- code/script.js
+++ code/script.js
@@ -1,4 +1,6 @@
 
+const spacing = 150;
+
 fetch("https://api.oyez.org/cases?filter=issue:423&page=0&per_page=0")
 .then(res=>res.json())
 .then(response=>{
@@ -17,7 +19,8 @@ fetch("https://api.oyez.org/cases?filter=issue:423&page=0&per_page=0")
             name: singleCase.name,
             ID: singleCase.ID,
             date: decidedDate.dates[0],
-            term: singleCase.term
+            term: singleCase.term,
+            caseURL: 'https://api.oyez.org/cases/' + singleCase.term + '/' + singleCase.docket_number
         };
     });
     
@@ -25,27 +28,65 @@ fetch("https://api.oyez.org/cases?filter=issue:423&page=0&per_page=0")
     return cases;
 })
 .then(caseList=>{
-    // this is creating an svg for timeline 
+    
+    window.addEventListener('scroll', e => {console.log(e.offsetY)})
+    
+    // this is creating an div for timeline 
     let timeline  = d3.select('#timeline')
-        .append('svg')
-        .attr('width', 800)
-        .attr('height', 1000);
+        .append('div')
+        .attr('width', 1000)
+        .attr('height', caseList.length*150) // 150 becasue of font size
+       
     
     // creating groups for each case and putting into timeline div 
-    let cases = timeline.selectAll('g.case')
+    let cases = timeline.selectAll('div.case')
            .data(caseList) // this is taking data from caseList 
            .enter()
-           .append('g')
+           .append('div')
            .attr('class', 'case');
 
     
     let titles = cases
-            .append('text')
+            .append('a')
             .attr('class', 'title')
-            .text((d) => { return d.term + " &mdash; " + d.name; })
+            .text((d) => { return d.term + " - " + d.name; })
+            .on("click", (d) => {
+                showCase(d) 
+            })
+    // titles.append('span')
+    //     .text('something')
 })
 
 
 .catch(error=>{
     console.log(error);
 });
+
+
+//_________________________________________________________________________//
+
+    
+    const showCase = (oneCase) => {
+        console.log(oneCase)
+        d3.select('body')
+        .attr('class', 'case');
+        fetch(oneCase.caseURL)
+            .then(res=>res.json())
+            .then(response=>{
+                console.log(response);
+        });
+        
+        fetch('cases/' + oneCase.ID + ".html")
+            .then(res=>res.text())
+            .then(response=>{
+                console.log(response);
+            d3.select('#facts')
+            .html(response)
+            .selectAll("a")
+            .on("click", (d) => {
+                alert(d) 
+            })
+        });
+    }
+    
+
